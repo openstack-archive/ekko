5bef024d6a2e61b609b892249f385c9aa8d73ed4
SamYaple <sam@yaple.net>
2016-01-04 23:11:01 +0000

    [WIP] Add manifest parser
    This reads the manifest. Its more based on osdk.py that I initially
    thought it would be. But hopefully I have cleaned it up a good bit.
    
    Update backup.py to take advantage of this.
    
    To generate a manifest with 2^24 objects (64TB for 4MB objects) run:
    python backup.py --fakeblockdev 64000 --manifest /pathtosave/manifest
    
    Change-Id: I09a8fb64c0d4b8e4b4b436cca44b57bec87116cd

--- tools/backup.py
+++ tools/backup.py
@@ -17,78 +17,109 @@
 # Copied and licensed from https://github.com/SamYaple/osdk
 
 
-from hashlib import sha1
-from osdk import osdk
+import argparse
+from collections import namedtuple
+# from hashlib import sha1
+import os
+import sys
 from uuid import uuid4 as uuid
+
+from ekko import manifest
+import six
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Backup Block Device')
+    blockdevloc = parser.add_mutually_exclusive_group(required=True)
+    blockdevloc.add_argument('--blockdev', required=False,
+                             help='Block device to backup')
+    blockdevloc.add_argument('--fakeblockdev', required=False, type=int,
+                              help='Fake a blockdevice (size in GB)')
+    parser.add_argument('--manifest', required=True,
+                        help='manifest file')
+    parser.add_argument('--cbt', required=False,
+                        help='change block tracking info')
+    return parser.parse_args()
 
 
 def get_disk_size(device):
-    with open(device, 'rb') as f:
-        return f.seek(0, 2)
+    f = os.open(device, os.O_RDONLY)
+    try:
+        return os.lseek(f, 0, os.SEEK_END)
+    finally:
+        os.close(f)
 
 
-def read_segments(f, lst, size, o):
-    zero_hash = sha1(bytes([0] * size)).hexdigest()
+def read_segments(f, lst, size, backup):
+    backup.segments = dict()
+    backup.hashes = dict()
+    Segment = namedtuple(
+        'Segment',
+        'base incremental compression encryption'
+    )
 
     for segment in lst:
-        f.seek(segment * size, 0)
-        data = f.read(size)
-        if not data:
-            raise Exception('Failed to read data')
+        # Do not actually do the backup
 
-        sha1_hash = sha1(data)
-        if sha1_hash.hexdigest() != zero_hash:
-            meta = dict()
-            meta['incremental'] = o.metadata['incremental']
-            meta['base'] = len(o.metadata['bases']) - 1
-            meta['encryption'] = 0
-            meta['compression'] = 0
-            meta['sha1_hash'] = sha1_hash.digest()
-            o.segments[segment] = meta
-        else:
-            try:
-                del o.segments[segment]
-            except KeyError:
-                pass
+        # f.seek(segment * size, 0)
+        # data = f.read(size)
+        # if not data:
+        #     raise Exception('Failed to read data')
+
+        backup.segments[segment] = Segment(
+            len(backup.metadata['bases']) - 1,
+            backup.metadata['info'].incremental,
+            0,
+            0
+        )
+        # backup.hashes[segment] = sha1(data).digest()
+        # Empty sha
+        backup.hashes[segment] = str.encode('\0\0' * 20)
+
+
+def check_manifest(manifest_file):
+    return os.path.isfile(manifest_file)
 
 
 def main():
-    device = '/dev/loop0'
-    old_manifest = 'manifest0.osdk'
-    manifest = 'manifest0.osdk'
-    manifest = 'manifest1.osdk'
+    args = parse_args()
     segment_size = 4 * 1024**2  # 4MiB
-    size_of_disk = get_disk_size(device)
+    if args.fakeblockdev:
+        size_of_disk = args.fakeblockdev * 1024**3  # Convert GB to B
+        args.blockdev
+    else:
+        size_of_disk = get_disk_size(args.blockdev)
     num_of_sectors = int(size_of_disk / 512)
     num_of_segments = int(size_of_disk / segment_size)
+    incremental = 0
 
-    o = osdk(manifest)
-    o.metadata['sectors'] = num_of_sectors
+    Info = namedtuple(
+        'Info',
+        'timestamp incremental segment_size sectors'
+    )
 
-    new = True
-    new = False
-    existing = True
-    existing_full = True
-    existing_full = False
+    if check_manifest(args.manifest):
+        print('manifest exists; exiting')
+        return
 
-    if new:
-        o.metadata['incremental'] = 0
-        o.metadata['segment_size'] = segment_size
-        o.metadata['bases'] = [uuid().bytes]
-        segments_to_read = range(0, num_of_segments - 1)
-    elif existing:
-        o.read_manifest(old_manifest)
-        o.metadata['incremental'] += 1
-        segments_to_read = range(1, num_of_segments - 1)
-    elif existing_full:
-        o.read_manifest(old_manifest)
-        o.metadata['incremental'] += 1
-        segments_to_read = range(0, num_of_segments - 1)
+    backup = manifest.Manifest(args.manifest)
 
-    with open(device, 'rb+') as f:
-        read_segments(f, segments_to_read, segment_size, o)
+    backup.metadata['info'] = Info(
+        manifest.utctimestamp(),
+        incremental,
+        segment_size,
+        num_of_sectors,
+    )
 
-    o.write_manifest()
+    backup.metadata['bases'] = [uuid().bytes]
+    segments_to_read = six.moves.range(0, num_of_segments - 1)
+
+    if args.fakeblockdev:
+        read_segments(args.fakeblockdev, segments_to_read, segment_size, backup)
+
+    # with open(args.blockdev, 'rb+') as f:
+    #    read_segments(f, segments_to_read, segment_size, backup)
+    backup.write_manifest()
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
