2e389f1ddb3765dd4e1f718220fdf8d2ec22dc92
SamYaple <sam@yaple.net>
2016-01-18 20:44:07 +0000

    Add storage code
    This code will be the basis of writing out the data to the backing
    storage with the initial driver being on disk storage.
    
    Additionally move compression/encryption code to storage rather than
    backup module
    
    ./backup.py --backup /dev/loop0 --manifest mani --location /path/to/store/backup/
    
    Change-Id: I22d291ea3ac03b4d5ddb447915c972db4cdc195d

---
+++ ekko/storage/_drivers/local.py
@@ -0,0 +1,43 @@
+# Copyright 2016 Sam Yaple
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from distutils.dir_util import mkpath
+import os
+import uuid
+
+from ekko.storage import drivers
+
+
+class LocalStorage(drivers.BaseStorage):
+
+    def put_data(self, data_segment):
+        data = data_segment[0]
+        segment = data_segment[1]
+        file_path = os.path.join(
+            self.storage_location,
+            str(uuid.UUID(bytes=segment.backupset_id)),
+            str(segment.incremental)
+        )
+
+        mkpath(file_path)
+
+        file_output = os.path.join(
+            file_path,
+            str(segment.segment)
+        )
+
+        with open(file_output, 'wb') as f:
+            f.write(data)
+
+        return segment
